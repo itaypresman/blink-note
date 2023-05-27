988f1d04434227f3bac8f95ea02b2bc5a10f2513
Itay Presman <presman.a.g@gmail.com>
2023-05-28 01:11:16 +0300

    Init frontend

---
+++ frontend/src/Application.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '@public/styles.css';
+// import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
+
+function Application() {
+    return (
+        <h1>Hello World</h1>
+    );
+}
+
+
+export default Application;
---
+++ frontend/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Application from './Application.jsx';
+
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(
+    <Application/>
+);
